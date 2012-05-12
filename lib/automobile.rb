699d06387dfcb37368882040ab57d9d7da5cfcbb
Peter Aronoff <telemachus@arpinum.org>
2012-05-12 15:46:39 -0400

    Panda level done

--- lib/automobile.rb
+++ lib/automobile.rb
@@ -1,18 +1,19 @@
 class Automobile
-  attr_accessor :color, :make, :model, :year
+  attr_accessor :color
+  attr_reader :make, :model, :year
 
   def self.wheels
     4
   end
 
-  def initialize(features)
+  def initialize(features = {})
     defaults = {
       :color => 'black',
       :make => 'ford',
       :model => 'taurus',
       :year => '2012'
     }
-    features.merge!(defaults)
+    features = defaults.merge(features)
 
     @color = features[:color]
     @make = features[:make]
--- test/test_automobile.rb
+++ test/test_automobile.rb
@@ -18,4 +18,40 @@ describe "Automobile class" do
     car.model.must_equal 'taurus'
     car.year.must_equal '2012'
   end
+
+  it 'should override defaults when given various values' do
+    white_car = Automobile.new({:color => 'white'})
+    mercedes = Automobile.new({:make => 'mercedes'})
+    charger = Automobile.new({:model => 'charger'})
+    classic = Automobile.new({:year => '1968'})
+
+    white_car.color.must_equal 'white'
+    mercedes.make.must_equal 'mercedes'
+    charger.model.must_equal 'charger'
+    classic.year.must_equal '1968'
+  end
+
+  it 'should accept multiple values in any order' do
+    old_charger = Automobile.new({:year => '1968', :model => 'charger'})
+    new_mercedes = Automobile.new({
+      :model => 'SE',
+      :make => 'mercedes',
+      :color => 'red',
+      :year => '2010'
+    })
+
+    old_charger.model.must_equal 'charger'
+    old_charger.year.must_equal '1968'
+
+    new_mercedes.color.must_equal 'red'
+    new_mercedes.year.must_equal '2010'
+  end
+
+  it 'should be able to change its color' do
+    car = Automobile.new
+    car.color.must_equal 'black'
+
+    car.color = 'white'
+    car.color.must_equal 'white'
+  end
 end
