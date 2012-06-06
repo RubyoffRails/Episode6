36fb192bd9697598b96737b66bd1fcfea8ad450f
Daniel Estrada <daniel.estrada98@gmail.com>
2012-06-05 20:35:38 -0400

    Episode 6

--- block_review.rb
+++ block_review.rb
@@ -1,7 +1,17 @@
 require 'rubygems'
 require 'bundler/setup'
 
-require_relative 'db/setup'
-Dir.glob("./**/*.rb").each {|f| require f}
+#require_relative 'db/setup'
+#Dir.glob("./**/*.rb").each {|f| require f}
+
+class Automobile 
+ 
+  def wheels
+     "4"
+  end
+end
+
+puts "number of wheels on your car: " + Automobile.new.wheels
+
 
 puts "Serenity now!"
