3ba841568ed2c55b0bfb3323b39019a346e6bc23
Anna Vester <veannaa@gmail.com>
2013-01-03 21:05:16 -0600

    Panda and Tiger

---
+++ models/automobile.rb
@@ -0,0 +1,23 @@
+class Automobile
+
+
+  def self.wheels?
+    4
+  end
+
+  def color(color)
+    @color = color
+  end
+
+  def make(make)
+    @make = make
+  end
+
+  def model(model)
+    @model = model
+  end
+
+  def year(year)
+    @year = year
+  end
+end
