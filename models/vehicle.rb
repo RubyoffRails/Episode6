c0105c9b11ab15e36187564da68c1f5c8ecf723b
mee213 <evans.marlo@gmail.com>
2014-01-22 14:18:50 -0600

    Eagle Assignment - first attempt at #1 & #2

--- models/vehicle.rb
+++ models/vehicle.rb
@@ -1,5 +1,21 @@
 class Vehicle
+
+	@@allvehicles = []
+
+	def initialize
+		@@allvehicles << self
+	end
+
 	def self.tires
 		4
 	end
+
+	def self.filter
+		@@allvehicles.select do |i|
+			i.color == "Blue"
+			i.make == "Honda"
+			i.model == "Accord"
+		end
+	end
+
 end
