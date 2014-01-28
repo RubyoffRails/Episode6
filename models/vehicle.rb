7f111f6be7e5beeae4551ef19bc79bc93e15cf12
Michael Vezzani <mjvezzani@gmail.com>
2014-01-28 13:27:45 -0500

    Fail test for tracking number of vehicles made. Unsure of where to go from here.

--- models/vehicle.rb
+++ models/vehicle.rb
@@ -1,5 +1,11 @@
 class Vehicle
 
+	@@vehicles_made ||= 0
+
+	def initialize
+		@@vehicles_made += 1
+	end
+
 	def self.number_of_wheels
 		4
 	end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -2,4 +2,7 @@ require 'spec_helper'
 
 describe Vehicle do
 	
+	it "tracks the number of vehicles created" do
+		Motorcycle.new.should change{@@vehicles_made}.by(1)
+	end
 end
