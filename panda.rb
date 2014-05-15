23a900600e824036c782b4af8294327126f9208c
Adam Lowe <tidalinfluence@gmail.com>
2014-05-14 19:42:29 -0700

    Started Panda assignment

---
+++ panda.rb
@@ -0,0 +1,16 @@
+class Vehicle
+  @@wheels = 4
+
+  attr_accessor :color, :make, :model, :color
+  def initialize
+    @color = color
+    @make = make
+    @model = model
+    @color = color
+  end
+
+  def wheels
+    @@wheels
+  end
+end
+
