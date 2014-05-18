d7986479d903f6db0b5caa432bc51e42a76c2fff
Dana Nourie <dananourie@gmail.com>
2014-05-17 19:04:04 -0700

    Keep getting a wrong number of arguments error, and I can't figure out why.

--- automobile.rb
+++ automobile.rb
@@ -1,22 +1,19 @@
 class Automobile
   @@wheels = 4
+  attr_accessor :color, :make, :model, :year
 
-  def initialize(color, make, year)
-    @color = color
-    @make = make
-    @year = year
+  def initialize(color, make, model, year)
+    @color = hash.fetch(:color, '')
+    @make = hash.fetch(:make, '')
+    @model = hash.fetch(:model, '')
+    @year = hash.fetch(:year, '')
+
   end
 
   def to_s
-    "My car is #{@color}, its make is #{@make}, and #{@year}"
-  end
-
-  def car
-    myride = { "color" => "blue", "make" => "chevy", "year" => :"2000" }
-    myride.each_value {|value| puts value }
+    "My car is #{@color}, its make and model is #{@make} and #{@model}, and the year is #{@year}."
   end
 end
 
-mycar = Automobile.new('Cyberblue', 'Chevy', 2012)
-puts mycar.to_s
-mycar.car
+mycar = Automobile.new([color: 'Cyberblue', make: 'Chevy', model: 'Volt', year:  '2012'])
+puts mycar
